6814598ac35807d4b732783774006a3f0677c2b8
liuyuhuan <liuyuhuan@babytree-inc.com>
2020-12-22 18:47:00 +0800

    flx

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,49 @@
+<template>
+  <svg
+    :class="svgClass"
+    aria-hidden="true"
+    :width="size.split(',')[0] + 'px'"
+    :height="size.split(',')[1] + 'px'"
+  >
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "svg-icon",
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String
+    },
+    size: {
+      type: String,
+      default: "24,24"
+    }
+  },
+  computed: {
+    iconName() {
+      return `#icon-${this.iconClass}`;
+    },
+    svgClass() {
+      if (this.className) {
+        return "svg-icon " + this.className;
+      } else {
+        return "svg-icon";
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.svg-icon {
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,34 +1,26 @@
 <template>
-  <div class="mian">
-    123
-  </div>
+	<div class="mian">
+		<svg-icon icon-class="dz" />
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				msg: 'Welcome to Your Vue.js App'
+			}
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	.mian {
+		position: relative;
+		width: 100%;
+		height: 100vh;
+		background: #2C3E50;
+	}
 </style>
